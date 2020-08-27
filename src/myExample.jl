5a9d30c2018cf57a62d405958c497ab4a06b2c57
Jin <jin.xu.2010@gmail.com>
2020-08-27 11:57:55 -0700

    Update myExample.jl

--- src/myExample.jl
+++ src/myExample.jl
@@ -1,9 +1,11 @@
 module myExample
 
+# using *** is here, even it get used in the extra_file.jl
+
 greet() = print("Hello World!")
 
 include("extra_file.jl")
 
 
-export my_f
+export my_f #, another function to export
 end # module
