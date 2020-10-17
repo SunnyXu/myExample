750c06aa2b4bfd9d2c157f070c2dfc2412228ce6
Jin <jin.xu.2010@gmail.com>
2020-10-16 17:32:35 -0700

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Documenter
+using myExample
+    sitename = "myExample",
+    format = Documenter.HTML(),
+    modules = [myExample]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
