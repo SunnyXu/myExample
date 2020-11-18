7b5dd01a4d83d84cfbd1e318cf38019b9da8af21
Jin <jin.xu.2010@gmail.com>
2020-11-18 14:49:11 -0800

    Deployed 250547a with MkDocs version: 1.1.2

---
+++ search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#myExample.jl","page":"Home","title":"myExample.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = myExample\nDocTestSetup = quote\n    using myExample\nend","category":"page"},{"location":"","page":"Home","title":"Home","text":"```@autodocs Modules = [myExample]","category":"page"}]
+}
